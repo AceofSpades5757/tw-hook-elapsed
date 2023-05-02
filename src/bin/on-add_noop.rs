686f7ac56802e5cf226420488e4bdaa9a7c9dfe3
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2023-05-02 00:20:38 -0500

    Fix output with trailing newline

--- src/bin/on-add_noop.rs
+++ src/bin/on-add_noop.rs
@@ -9,9 +9,10 @@ fn main() {
     let lines: String = stdin.lock().lines().map(|l| l.unwrap()).collect();
 
     let task: Task = lines.into();
+    dbg!(&task);
 
     // Write modified task JSON to stdout
     let stdout = io::stdout();
     let mut handle = stdout.lock();
-    handle.write_all(task.to_json().as_bytes()).unwrap();
+    handle.write_all(format!("{}\n", task.to_json()).as_bytes()).unwrap();
 }
