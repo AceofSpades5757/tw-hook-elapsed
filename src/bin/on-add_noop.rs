1af6704442c694336f2da0a4e50bc295f0b77ffe
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2023-05-01 23:49:43 -0500

    Add noop hook

---
+++ src/bin/on-add_noop.rs
@@ -0,0 +1,17 @@
+use std::io::{self, Write, BufRead};
+
+use tasklib::Task;
+
+fn main() {
+
+    // Read task JSON from stdin
+    let stdin = io::stdin();
+    let lines: String = stdin.lock().lines().map(|l| l.unwrap()).collect();
+
+    let task: Task = lines.into();
+
+    // Write modified task JSON to stdout
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    handle.write_all(task.to_json().as_bytes()).unwrap();
+}
