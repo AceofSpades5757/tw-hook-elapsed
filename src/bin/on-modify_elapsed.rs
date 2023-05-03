efc1c2fc195af2d195570db83fb69fb8e176a3cf
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2023-05-03 15:07:29 -0500

    Update project to first, complete working version

--- src/bin/on-modify_elapsed.rs
+++ src/bin/on-modify_elapsed.rs
@@ -1,3 +1,31 @@
+//! Add UDA elapsed time to modified tasks.
+//!
+//! Adds time to existing time if an existing time is present.
+
+use tasklib::prelude::*;
+
+use elapsed::add_elapsed;
+
 fn main() {
-    todo!()
+    env_logger::init();
+
+    // Read task JSON from stdin
+    let pre_task: Task = Task::from_stdin().unwrap();
+    log::info!("task: {:?}", &pre_task);
+    let post_task: Task = Task::from_stdin().unwrap();
+    log::info!("task: {:?}", &post_task);
+
+    let start = post_task.start();
+    let end = post_task.end();
+
+    if start.is_none() || end.is_none() {
+        log::info!("start or end is None");
+        return;
+    }
+
+    let modified_task = add_elapsed(post_task);
+    log::info!("post_task: {:?}", &modified_task);
+
+    // Write modified task JSON to stdout
+    modified_task.to_stdout().unwrap();
 }
