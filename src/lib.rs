1a70d6f020bf27ed7378465d3ecf42c60d183a00
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-03-30 01:12:28 -0500

    refactor: disable adding existing elapsed time

--- src/lib.rs
+++ src/lib.rs
@@ -10,23 +10,24 @@ pub fn add_elapsed(task: Task) -> Task {
 
     let start = start.unwrap();
     let end = end.unwrap();
-    let existing_elapsed = task
-        .udas()
-        .get("elapsed")
-        .unwrap_or(&UdaValue::Duration(Duration::days(0)))
-        .clone();
-    let existing_elapsed: Duration = existing_elapsed.try_into().unwrap();
+    //let existing_elapsed = task
+    //.udas()
+    //.get("elapsed")
+    //.unwrap_or(&UdaValue::Duration(Duration::days(0)))
+    //.clone();
+    //let existing_elapsed: Duration = existing_elapsed.try_into().unwrap();
 
     let duration = end.signed_duration_since(*start);
     let elapsed: Duration = duration.into();
 
-    let total_elapsed = existing_elapsed + elapsed;
+    //let total_elapsed = existing_elapsed + elapsed;
 
     // UDAs come out just like any other value.
     let mut modified_task = task.clone();
     modified_task
         .udas_mut()
-        .insert("elapsed".into(), total_elapsed.into());
+        //.insert("elapsed".into(), total_elapsed.into());
+        .insert("elapsed".into(), elapsed.into());
 
     modified_task
 }
